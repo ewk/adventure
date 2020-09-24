f33227e5b6c6ed33fa4893ba3f2166f39451e83a
Eddie Kovsky <kovskye@oregonstate.edu>
2020-09-23 21:38:58 -0600

    Initial commit
    - Define a room struct (with some guesses about fields that will be
      useful later to connect rooms).
    - A game play object, for now, only has one attribute: a name.
    - Implement function loadRooms(). This will take a room definition
      formatted in JSON, import it as a room struct, and store the new
      struct in an array of rooms. The JSON files must be stored in
      the directory /rooms.

---
+++ adventure.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"regexp"
+)
+
+// Some necessary globals
+const MaxConnections = 6
+const MaxRooms = 15
+const MaxObjects = 8
+
+var rooms []Room // graph of rooms
+
+// definition of a room
+type Room struct {
+	Name      string
+	RoomType  string
+	Feature1  string
+	Feature2  string
+	LongDesc  string
+	ShortDesc string
+	Visited   bool
+	Count     int                   // number of connections
+	Out       [MaxConnections]*Room // outbound connections
+	objects   []*Object             // objects in this room
+}
+
+// game play object
+type Object struct {
+	Name string
+}
+
+// loadRooms reads room definitions from local storage and creates a
+// corresponding Room struct. Rooms must be defined as JSON and saved to the
+// 'rooms' directory relative to the game's home directory.
+func loadRooms() {
+	files, e := ioutil.ReadDir("rooms")
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	for _, f := range files {
+		matched, _ := regexp.MatchString(`\.json`, f.Name())
+		if matched == true {
+			roomJson, e := ioutil.ReadFile("rooms/" + f.Name())
+
+			if e != nil {
+				log.Fatal(e)
+			}
+
+			var r Room
+			json.Unmarshal([]byte(roomJson), &r)
+			rooms = append(rooms, r)
+		}
+	}
+	// Debug: uncomment to show that JSON data is now a struct in rooms array
+	//fmt.Printf("length=%d capacity=%d %v\n", len(rooms), cap(rooms), rooms)
+}
+
+func main() {
+	loadRooms()
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ewk/adventure
+
+go 1.15
